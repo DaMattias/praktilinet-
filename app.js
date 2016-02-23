30fda50813d40c9e8483bc3c1912b2b92b87fc9c
Mattias Mihkelson <mattyas1997@gmail.com>
2016-02-23 12:32:10 +0200

    lisasin kuvamise

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ var ctx = document.getElementById('canvas').getContext('2d');
 
 var radians = function(degrees) {
   return Math.PI * degrees / 180
-}
+};
 
 var degrees = function(radians) {
   return radians * 180 / Math.PI
-}
+};
+
+
+var xValueElement = document.getElementById('value-x');
+var vValueElement = document.getElementById('value-speed');
+var FresValueElement = document.getElementById('value-fres');
+var FhValueElement = document.getElementById('value-fh');
+var aValueElement = document.getElementById("value-a");
 
 var width = canvas.getAttribute('width'),
     height = canvas.getAttribute('height'),
     g = 9.807,
     mass = 2,
     my = 0.7,
-    vi = -5,
-    angle = radians(28)
+    vi = -10,
+    angle = radians(28),
     pixelsByMeter=20;
 
 ctx.translate(0, height);
@@ -34,7 +41,7 @@ var Kolmnurk = function(ctx, fillStyle) {
       this.a = height-keha.h;
       this.b = this.a / Math.tan(angle);
     }
-  }
+  };
 
   this.fillStyle = typeof fillStyle !== "undefined" ? fillStyle : "rgb(200,200,200)";
 
@@ -43,14 +50,14 @@ var Kolmnurk = function(ctx, fillStyle) {
     this.ctx.save();
     this.ctx.fillStyle = this.fillStyle;
     this.ctx.beginPath();
-    this.ctx.moveTo(this.b, 0)
+    this.ctx.moveTo(this.b, 0);
     this.ctx.lineTo(0,0);
     this.ctx.lineTo(0, this.a);
     this.ctx.closePath();
     this.ctx.fill();
     this.ctx.restore();
   }
-}
+};
 
 var Keha = function(ctx, w, h, fillStyle) {
   this.ctx = ctx;
@@ -62,19 +69,19 @@ var Keha = function(ctx, w, h, fillStyle) {
 
   this.setX = function(x) {
     this.x = x;
-  }
+  };
 
   this.calcFx = function() {
     this.Fx = mass*g*Math.sin(angle);
-  }
+  };
 
   this.calcFn = function() {
     this.Fn = mass*g*Math.cos(angle);
-  }
+  };
 
   this.calcCritAngle = function() {
     this.critAngle = Math.atan(my);
-  }
+  };
 
   this.calcFh = function() {
     this.calcFn();
@@ -84,38 +91,38 @@ var Keha = function(ctx, w, h, fillStyle) {
     } else if (vi != 0) {
       this.Fh = Math.sign(vi) * my * this.Fn
     }
-  }
+  };
 
   this.calcFRes = function() {
     this.calcFx();
     this.calcFh();
     this.FRes = this.Fx-this.Fh;
-  }
+  };
 
   this.calcA = function() {
     this.calcFRes();
     this.A = this.FRes / mass;
-  }
+  };
 
   this.calcNurk = function() {
     if (angle<Math.atan((height-keha.h)/width)) {
-      this.b = width
+      this.b = width;
       this.a = this.b*Math.tan(angle);
     } else {
       this.a = height-keha.h;
       this.b = this.a / Math.tan(angle);
     }
-  }
+  };
 
   this.calcSpeed = function(t) {
     this.calcA();
     this.Speed = vi + this.A*t
-  }
+  };
 
   this.calcPos = function(t) {
     this.calcSpeed(t);
     this.x = pixelsByMeter * this.Speed * t;
-  }
+  };
 
 
   this.fillStyle = typeof fillStyle !== "undefined" ? fillStyle : "rgb(100,100,100)";
@@ -139,13 +146,21 @@ var Keha = function(ctx, w, h, fillStyle) {
     */
     this.ctx.restore();
   }
-}
+};
 
 var keha = new Keha(ctx, 80, 50);
 var kolmnurk = new Kolmnurk(ctx);
 keha.draw();
 
-var x2 = 0
+var x2 = 0;
+
+var showInfo = function(ctx, block) {
+  xValueElement.innerHTML = Math.round(keha.x);
+  vValueElement.innerHTML = Math.round(keha.Speed);
+  aValueElement.innerHTML = Math.round(keha.a);
+  FresValueElement.innerHTML = Math.round(keha.FRes);
+  FhValueElement.innerHTML = Math.round(keha.Fh);
+}
 
 var step = function(timeStamp) {
   ctx.clearRect(0, 0, width, height);
@@ -155,8 +170,9 @@ var step = function(timeStamp) {
     //keha.Fh = keha.Fh*(-1)
     console.log('tru')
   }
-  keha.calcPos(t)
+  keha.calcPos(t);
   keha.draw();
+  showInfo();
   if (vi < 0) {
     if (keha.x <= kolmnurk.a/Math.sin(angle)-keha.w*2) {
       window.requestAnimationFrame(step);
@@ -167,6 +183,6 @@ var step = function(timeStamp) {
     }
   }
   x2 = keha.x;
-}
+};
 
 window.requestAnimationFrame(step);
